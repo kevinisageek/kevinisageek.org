442c5caa2a1ba41a9566dbd86fc893cac5c716b8
kevinisageek <kevin@kevinisageek.org>
2017-02-26 12:28:26 +0000

    Changed the signature to ascii armored file

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITENAME = u'Random Geekage'
 SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
-STATIC_PATHS = ['misc','images','content','id.txt','id.txt.gpg','keybase.txt']
+STATIC_PATHS = ['misc','images','content','id.txt','id.txt.asc','keybase.txt']
 
 TIMEZONE = 'Europe/London'
 
