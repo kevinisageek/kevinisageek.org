280c4d8eb51c98ce1b155cad1a24c6fbafcd0b81
kevinisageek <kevin@kevinisageek.org>
2018-01-15 09:01:08 +0000

    Added Dash/Plus extension, moved images.

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITENAME = u'Random Geekage'
 SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
-STATIC_PATHS = ['misc','images','content','id.txt','id.txt.asc','keybase.txt','kevinatkevinisageekdotorg.gpg.asc']
+STATIC_PATHS = ['misc','images','content','id.txt','id.txt.asc','keybase.txt','kevinatkevinisageekdotorg.gpg.asc', '*.jpg']
 
 TIMEZONE = 'Europe/London'
 
