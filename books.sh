a199cea742558387877efcf3bba1be188575e505
Kevin <kevin@kevinisageek.org>
2019-07-06 09:58:43 +0100

    Books cosmetic tweaks

--- books.sh
+++ books.sh
@@ -1,10 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash
 
 cd "$(dirname "$0")"
-
-calibredb catalog content/media/library/books.csv --fields=id,author_sort,title,isbn,identifiers,series,series_index,uuid --search="#read:yes"
-
-#gsed -i '1 s/[\d128-\d255]//g' content/media/misc/books.csv
-
+calibredb catalog content/media/library/books.csv --fields=id,authors,title,isbn,identifiers,series,series_index,uuid --search="#read:yes"
 csvjson -i 4 content/media/library/books.csv > content/media/library/books.json
 cd "$OLDPWD"
