36a17ca52d738d5f125010d28585d3f375b3e340
Kevin <kevin@kevinisageek.org>
2020-07-22 08:12:58 +0100

    Added photo galleries created by mkgallery

--- books.sh
+++ books.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")"
 calibredb catalog content/media/library/books.csv --fields=authors,title,isbn,series,series_index --search="#read:yes"
-csv2mdtab content/media/library/books.csv content/media/library/books.md
+#csv2mdtab content/media/library/books.csv content/media/library/books.md
 csvjson content/media/library/books.csv > content/media/library/books.json
 cd "$OLDPWD"
