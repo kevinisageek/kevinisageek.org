ca7fa5266cb0049e221874d9532838cdc4373481
Kevin <kevin@kevinisageek.org>
2021-01-30 14:17:43 +0000

    Switching theme and crossing fingers

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,7 +19,7 @@ AUTHOR_FEED_RSS = None
 AUTHOR_SAVE_AS = ''
 AUTHOR_URL = ''
 PLUGIN_PATHS = ['plugins']
-#PLUGINS = ['tipue_search']
+PLUGINS = ['tipue_search']
 DIRECT_TEMPLATES = ('index', 'tags', 'archives', 'search')
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
@@ -28,17 +28,17 @@ PAGE_SAVE_AS = '{slug}/index.html'
 TAG_URL = 'tag/{slug}/'
 TAG_SAVE_AS = 'tag/{slug}/index.html'
 STATIC_SAVE_AS = '{path}'
-#SEARCH_SAVE_AS = 'search/index.html'
-#SEARCH_URL = 'search/'
+SEARCH_SAVE_AS = 'search/index.html'
+SEARCH_URL = 'search/'
 DISPLAY_PAGES_ON_MENU = True
 HIDE_SIDEBAR = True
-THEME = 'themes/pelican-kevinisageek'
+THEME = 'themes/jlelse'
 IGNORE_FILES = ['.#*','.DS_Store']
 DEFAULT_LANG = 'en'
 MENUITEMS = (
-			('Feed', SITEURL + '/feed'),
-			('Photos', SITEURL + '/media/images')
-			)
+	('Feed', SITEURL + '/feed'),
+	('Photos', SITEURL + '/media/images')
+)
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
 READERS = {'html': None}
