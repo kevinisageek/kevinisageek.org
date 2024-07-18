10e9e8d75faf387052aab58fe538928fff772bf4
Kevin <kevin@kevinisageek.org>
2024-07-18 07:07:38 +0100

    Add initial OpenGraph support, jsonfeed is broken so remove it for now

--- config.py
+++ config.py
@@ -21,7 +21,8 @@ FEED_JSON = 'feed/index.json'
 AUTHOR_SAVE_AS = ''
 AUTHOR_URL = ''
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ['tipue_search', 'pelican-jsonfeed']
+PLUGINS = ['tipue_search', #'pelican-jsonfeed'
+]
 DIRECT_TEMPLATES = ('index', 'tags', 'archives', 'search')
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
@@ -36,14 +37,16 @@ TAGS_SAVE_AS = 'tags/index.html'
 STATIC_SAVE_AS = '{path}'
 SEARCH_SAVE_AS = 'search/index.html'
 SEARCH_URL = 'search/'
+SUMMARY_MAX_LENGTH = 15
 DISPLAY_PAGES_ON_MENU = True
 HIDE_SIDEBAR = True
 THEME = 'themes/goblog'
 IGNORE_FILES = ['.#*','.DS_Store']
 DEFAULT_LANG = 'en'
+DEFAULT_METADATA = {"fedicreator": "@kevin@fedi.kevinisageek.org"}
 MENUITEMS = (
 	('Atom Feed', SITEURL + '/feed'),
-	('JSON Feed', SITEURL + '/feed/index.json'),
+	#('JSON Feed', SITEURL + '/feed/index.json'),
 	('Photos', SITEURL + '/media/images'),
 )
 RELLINKS = (
